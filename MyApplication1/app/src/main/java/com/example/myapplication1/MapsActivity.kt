e6204c552a0b086255dee18c8b780c6ffa87ffa4
Juan Lopez <juan.lopez08@epn.edu.ec>
2021-04-09 17:42:28 -0500

    Marcadores agregados con el boton

--- MyApplication1/app/src/main/java/com/example/myapplication1/MapsActivity.kt
+++ MyApplication1/app/src/main/java/com/example/myapplication1/MapsActivity.kt
@@ -2,19 +2,23 @@ package com.example.myapplication1
 
 import android.content.pm.PackageManager
 import android.location.Location
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_maps.*
+import java.security.AccessController.getContext
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
@@ -45,19 +49,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-
-
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-
-
-
         mMap.setOnMarkerClickListener(this)
         mMap.uiSettings.isZoomControlsEnabled = true
 
         setUpMap()
+        agregarMarcadores()
 
     }
 
@@ -77,6 +73,28 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
                 mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLong, 13f))
             }
         }
+    }
+
+    private fun agregarMarcadores(){
+        // Add a marker in Sydney and move the camera
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+
+        floatingButton.setOnClickListener(){
+
+            mMap.setOnMapClickListener(OnMapClickListener { point ->
+                //Toast.makeText(this, point.latitude.toString() + ", " + point.longitude, Toast.LENGTH_SHORT).show()
+                val locationClicked = LatLng(point.latitude, point.longitude)
+                Toast.makeText(this, locationClicked.toString(), Toast.LENGTH_SHORT).show()
+
+                mMap.addMarker(MarkerOptions().position(locationClicked))
+                //mMap.addMarker(MarkerOptions().position(LatLng(Location)).title("Probando"))
+                mMap.setOnMapClickListener(null)
+            })
+
+        }
+
     }
 
 
