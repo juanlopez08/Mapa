bf407849bccd39dcfc77ae1f34d5c5ba4bbc5eac
Juan Lopez <juan.lopez08@epn.edu.ec>
2021-04-09 12:37:25 -0500

    Login con firebase funcional

---
+++ MyApplication1/app/src/main/java/com/example/myapplication1/HomeActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        setup()
+    }
+
+    private fun setup(){
+            title="Inicio"
+
+    }
+
+}
---
+++ MyApplication1/app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.myapplication1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setup()
+    }
+
+    private fun setup(){
+        title="Autenticación"
+        signUpButton.setOnClickListener{
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()){
+
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(emailEditText.text.toString(), passwordEditText.text.toString()).addOnCompleteListener{
+                    if(it.isSuccessful){
+                        abrirHome()
+                    } else {
+                        alertar()
+                    }
+                }
+            }
+        }
+
+        loginButton.setOnClickListener{
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()){
+
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(emailEditText.text.toString(),
+                        passwordEditText.text.toString()).addOnCompleteListener{
+                    if(it.isSuccessful){
+                        abrirHome()
+                    } else {
+                        alertar()
+                    }
+                }
+            }
+        }
+
+    }
+
+    private fun alertar(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Autenticacion fallida")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog=builder.create()
+        dialog.show()
+    }
+
+    private fun abrirHome(){
+        val homeIntent = Intent(this, HomeActivity::class.java)
+        startActivity(homeIntent)
+    }
+
+}
+
