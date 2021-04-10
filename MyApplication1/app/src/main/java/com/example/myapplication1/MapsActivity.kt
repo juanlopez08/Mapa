9c13f25a6c30f1727b5adcd6f940585270f6b8fc
Juan Lopez <juan.lopez08@epn.edu.ec>
2021-04-10 02:15:34 -0500

    Obteniendo marcadores de firebase

--- MyApplication1/app/src/main/java/com/example/myapplication1/MapsActivity.kt
+++ MyApplication1/app/src/main/java/com/example/myapplication1/MapsActivity.kt
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
 import android.text.InputType
+import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -20,8 +21,7 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_maps.*
 
 
@@ -31,7 +31,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var lastLocation: Location
     val myDatabase = FirebaseDatabase.getInstance().getReference("Coordenadas")
-
+    var markedName=""
+    lateinit var locationClicked: LatLng
 
     companion object{
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1
@@ -70,7 +71,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
         ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
          return
      }
-
+        // cargar ubicacion del usuario
         mMap.isMyLocationEnabled = true
         fusedLocationClient.lastLocation.addOnSuccessListener(this){location ->
             if(location != null){
@@ -79,10 +80,59 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
                 mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLong, 4f))
             }
         }
+//        val marcadores = arrayOf(LatLng(-184.0, 151.0), LatLng(-34.0, 151.0),  LatLng(-34.0, 152.0))
+//        val titulos = arrayOf("1", "2", "3")
+//        for(posicion in marcadores.indices ){
+//            mMap.addMarker(MarkerOptions().position(marcadores[posicion]).title(titulos[posicion]))
+//        }
+
+       // Toast.makeText(this, "${myDatabase.key}", Toast.LENGTH_LONG).show()
+
+        myDatabase.addValueEventListener(object : ValueEventListener{
+            override fun onCancelled(error: DatabaseError) {
+                return
+            }
+
+            // GET VALUES FROM FIREBASE
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val mapaCoordenadas = snapshot.getValue()
+                Log.i("TAG2", "Got value ${mapaCoordenadas}")
+
+                if(mapaCoordenadas is Map<*, *>) {
+                    println("mapaCoordenadas is of type Map")
+                   // print("-------------------------------------")
+                   // print(mapaCoordenadas.size)
+                    print("--------------FOR EACH-----------------------")
+
+                    for ((k, v) in mapaCoordenadas) {
+                        print("--------------FOR-----------------------")
+                        println("$k = $v")
+                        if(v is Map<*,*>){
+                            var lat = v["latitude"]
+                            var lng = v["longitude"]
+                            mMap.addMarker(MarkerOptions().position(LatLng(lat as Double, lng as Double)).title(k as String?))
+//                            mMap.addMarker(MarkerOptions().position(LatLng(10.33,-66.83)).title(k as String?))
+                        }
+                    }
+
+//                    mapaCoordenadas.forEach {
+//                        k, v ->
+//                        println("$k = $v")
+//                        print("--------------AQUIIIII-----------------------")
+//                        print(v)
+//                        mMap.addMarker(MarkerOptions().position(LatLng(10.33,-66.83)).title(k as String?))
+//                    }
+
+
+
+                }
+            }
+        })
+
     }
 
     private fun agregarMarcadores(){
-        
+
         floatingButton.setOnClickListener(){
             //Dialogo para el titulo
             val builder: AlertDialog.Builder = android.app.AlertDialog.Builder(this)
@@ -97,17 +147,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
             // botones
             builder.setPositiveButton("OK", DialogInterface.OnClickListener { dialog, which ->
                 // Here you get get input text from the Edittext
-                var markedName = input.text.toString()
+                markedName = input.text.toString()
                 //Marcador
                 mMap.setOnMapClickListener(OnMapClickListener { point ->
                     //Toast.makeText(this, point.latitude.toString() + ", " + point.longitude, Toast.LENGTH_SHORT).show()
-                    val locationClicked = LatLng(point.latitude, point.longitude)
+                    locationClicked = LatLng(point.latitude, point.longitude)
                     //Toast.makeText(this, locationClicked.toString(), Toast.LENGTH_SHORT).show()
 
                     //Crear marcador
                     mMap.addMarker(MarkerOptions().position(locationClicked).title(markedName))
 
                     mMap.setOnMapClickListener(null)
+
 
                     //mandar datos a firebase ESCRITURA
                     myDatabase.child(markedName).setValue(locationClicked).addOnCompleteListener{
@@ -121,8 +172,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
             builder.show()
         }
     }
-
-
 
 
 }
